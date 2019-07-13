8b784b4c2a3d3ccb04f0c6bb1f706d2be246a536
Jerome Haltom <jhaltom@fileandserve.com>
2019-07-13 15:11:10 -0500

    Again.

--- Traefik/ApplicationPackageRoot/Traefik/Code/traefik.bat
+++ Traefik/ApplicationPackageRoot/Traefik/Code/traefik.bat
@@ -1,2 +1,2 @@
 
-powershell .\traefik.ps1 -ConfigTemplate .\traefik.toml -ClusterManagementUrl %ClusterManagementUrl%
+powershell .\traefik.ps1 -ConfigTemplate .\traefik.toml -TraefikPort %Fabric_Endpoint_TraefikEndpoint% -TraefikApiPort %Fabric_Endpoint_TraefikApiEndpoint% -ClusterManagementUrl %ClusterManagementUrl%
