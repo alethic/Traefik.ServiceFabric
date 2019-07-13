6d3b015fface1a20ee3ed3d4a4f43d9ba303be4f
Jerome Haltom <jhaltom@fileandserve.com>
2019-07-13 15:01:24 -0500

    Fix bat file.

--- Traefik/ApplicationPackageRoot/Traefik.Watchdog/Code/traefik-appinsights-watchdog.bat
+++ Traefik/ApplicationPackageRoot/Traefik.Watchdog/Code/traefik-appinsights-watchdog.bat
@@ -1,2 +1,2 @@
 ﻿
-.\traefik-appinsights-watchdog.exe --appinsightskey=%AppInsightsKey% --traefikbackendname=%Fabric_ServiceName% --traefikhealthendpoint=http://localhost:%TraefikApiPort%/health --watchdogtestserverport=%Fabric_Endpoint_WatchdogTestService% --watchdogtraefikurl=http://localhost:%TraefikPort%/Traefik/Watchdog --pollintervalsec=%PollIntervalSec% --debug=true
+.\traefik-appinsights-watchdog.exe --appinsightskey=%AppInsightsKey% --traefikbackendname=%Fabric_ServiceName% --traefikhealthendpoint=http://localhost:%TraefikApiPort%/health --watchdogtestserverport=%Fabric_Endpoint_ServiceEndpoint% --watchdogtraefikurl=http://localhost:%TraefikPort%/Traefik/Watchdog --pollintervalsec=%PollIntervalSec% --debug=true
