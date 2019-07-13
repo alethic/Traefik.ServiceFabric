988a1d438370337e0831f4924a161d6160f03c3f
Jerome Haltom <jhaltom@fileandserve.com>
2019-07-13 11:58:29 -0500

    Ignore EXE.

---
+++ Traefik/ApplicationPackageRoot/Watchdog/Code/traefik-appinsights-watchdog.bat
@@ -0,0 +1,2 @@
+﻿
+.\traefik-appinsights-watchdog.exe --appinsightskey=%AppInsightsKey% --traefikbackendname=%Fabric_ServiceName% --traefikhealthendpoint=http://localhost:%TraefikApiPort%/health --watchdogtestserverport=%Fabric_Endpoint_WatchdogTestService% --watchdogtraefikurl=http://localhost:%TraefikPort%/Traefik/Watchdog --pollintervalsec=%PollIntervalSec% --debug=true
