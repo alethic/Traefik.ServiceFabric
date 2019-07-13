7a18b3c63b67f8e3e8c24ba566501357d7017897
Jerome Haltom <jhaltom@fileandserve.com>
2019-07-13 15:10:01 -0500

    More replace strings.

--- Traefik/ApplicationPackageRoot/Traefik/Code/traefik.ps1
+++ Traefik/ApplicationPackageRoot/Traefik/Code/traefik.ps1
@@ -4,11 +4,19 @@ param(
     [string]$ConfigTemplate,
 
     [Parameter(Mandatory)]
+    [int]$TraefikPort,
+
+    [Parameter(Mandatory)]
+    [int]$TraefikApiPort,
+
+    [Parameter(Mandatory)]
     [string]$ClusterManagementUrl
 
 )
 
 $cfg = gc $ConfigTemplate
+$cfg = $cfg.Replace('%TraefikPort%', $TraefikPort)
+$cfg = $cfg.Replace('%TraefikApiPort%', $TraefikApiPort)
 $cfg = $cfg.Replace('%ClusterManagementUrl%', $ClusterManagementUrl)
 [IO.File]::WriteAllLines("$env:Fabric_Folder_App_Work\traefik.toml", $cfg)
 
